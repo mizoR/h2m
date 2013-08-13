df796ac5a4ab6da09e28ccdfe22022114c84a59f
mizoryu <suzunatsu@yahoo.com>
2013-08-13 22:42:31 +0900

    use LF (NOT CR+LF)

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @html = params[:html]
+    @html = params[:html].gsub("\r\n", "\n")
     @markdown = ReverseMarkdown.parse @html, :github_style_code_blocks => true
     slim :index
   end
